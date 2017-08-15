b32d177a2b7f8a8d2e30b585130a41df9ab67e56
edwork <ed.boal@edwork.org>
2017-08-15 12:13:03 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RPI-MQTT-JSON-Multisensor
 Raspberry Pi MQTT JSON Multisensor for Home Assistant. Supported sensors include the AM312 PIR, DHT11/22 temperature/humidity sensors.
+
+## Requirements
+* paho-mqtt (install using [pip](https://pypi.python.org/pypi/paho-mqtt/1.1))
+* Adafruit_DHT (install via their [git repo](https://github.com/adafruit/Adafruit_Python_DHT))
+
+## Running
+You can simply run the file in a terminal and go on your merry way or run it as a service.
+
+## Running as a service
+### Systemd
+* Copy rpi-sensor.service to `/etc/systemd/system/`
+* Run (as root) `systemctl enable rpi-sensor.service`
+* Run (as root) `systemctl start rpi-sensor.service`
---
+++ multisensor.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+########################################################
+__author__ = "Edward Boal <ed.boal@edwork.org>"
+__license__ = "GPL3"
+__version__ = "1.0"
+
+### RPI-MQTT-JSON-Multisensor
+#
+# Setup: Fill in variables under the configuration section 
+# See following for more information: https://github.com/edwork/RPI-MQTT-JSON-Multisensor
+#
+########################################################
+
+import time
+import json
+import paho.mqtt.publish as publish
+import Adafruit_DHT as dht
+from gpiozero import MotionSensor
+
+## Configuration
+# MQTT Server Information
+MQTT_HOST = 'ip-of-your-mqtt-server'
+MQTT_PORT = 1883
+MQTT_USER = 'noobuser'
+MQTT_PASSWORD = 'lamepass'
+MQTT_CLIENT_ID = 'pi-sensor-1'
+MQTT_TOPIC_PREFIX = 'hass/pisensornode'
+## Sensor Information
+TEMP_SENSOR_TYPE = 'DHT11'
+TEMP_SENSOR_PIN = 17 ## GPIO PIN
+MOTION_SENSOR_PIN = 4 ## GPIO PIN
+
+## Setup
+sensor_data = {}
+pir = MotionSensor(MOTION_SENSOR_PIN)
+dht_sensor = ('dht.' + TEMP_SENSOR_TYPE)
+auth_info = {
+  'username':MQTT_USER,
+  'password':MQTT_PASSWORD
+}
+try:
+    while True:
+        humidity,temperature = dht.read_retry(dht.DHT11,
+                                              TEMP_SENSOR_PIN
+                                              )
+        humidity = round(humidity, 3) ## Round to 3 places
+        temperature = round(temperature, 3) ## Round to 3 places
+        if pir.motion_detected:
+          motion = 1
+        else:
+          motion = 0
+        sensor_data['temperature'] = temperature
+        sensor_data['humidity'] = humidity
+        sensor_data['motion'] = motion
+        ## Publish the message to the MQTT Broker
+        publish.single('hass/testpi/sensor',
+                        json.dumps(sensor_data),
+                        hostname = MQTT_HOST,
+                        client_id = MQTT_CLIENT_ID,
+                        auth = auth_info,
+                        port = MQTT_PORT
+                       )
+        ## Delay before running again
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
